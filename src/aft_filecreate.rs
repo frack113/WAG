f87eb111295a7da82d0fa55fff4f875458413917
frack113 <62423083+frack113@users.noreply.github.com>
2023-09-02 17:20:34 +0200

    use rustfmt

--- src/aft_filecreate.rs
+++ src/aft_filecreate.rs
@@ -16,8 +16,8 @@ You can use `SET | more` or `Get-ChildItem Env:` to get the list
 
 use crate::tools_generator;
 
-use std::collections::{HashMap,HashSet};
 use serde::Deserialize;
+use std::collections::{HashMap, HashSet};
 
 use std::env;
 
@@ -34,10 +34,10 @@ struct JsonHeaderInfo {
 struct JsonPayloadInfo {
     name: String,
     needroot: bool,
-    file_type:String,
+    file_type: String,
     fullpath: String,
     cmd_var: String,
-    cmd_path: String
+    cmd_path: String,
 }
 
 #[derive(Deserialize)]
@@ -61,136 +61,143 @@ struct JsonGlobalInfo {
     split ads struct into 2 Hashmap
 */
 #[derive(Clone)]
-pub struct PayloadPathInfo{
+pub struct PayloadPathInfo {
     needroot: bool,
-    file_type:String,
-    fullpath:String,
-    cmd_var:String,
-    cmd_path:String,
+    file_type: String,
+    fullpath: String,
+    cmd_var: String,
+    cmd_path: String,
 }
 
 #[derive(Clone)]
-pub struct FileArtefac{
+pub struct FileArtefac {
     magicbyte: HashMap<String, String>,
-    payload: HashMap<String,PayloadPathInfo>,
+    payload: HashMap<String, PayloadPathInfo>,
     ads_adsname: HashMap<String, String>,
     ads_hexvalue: HashMap<String, String>,
 }
 
-impl FileArtefac{
-    pub fn new( ) -> Self { 
-        Self { 
+impl FileArtefac {
+    pub fn new() -> Self {
+        Self {
             magicbyte: HashMap::new(),
-            payload:HashMap::new(),
-            ads_adsname:HashMap::new(),
-            ads_hexvalue:HashMap::new()
-        } 
+            payload: HashMap::new(),
+            ads_adsname: HashMap::new(),
+            ads_hexvalue: HashMap::new(),
+        }
     }
 
-    pub fn load( &mut self,path:&str){
-        let file_path = std::env::current_dir().expect("Failed to get current folder").join(path);
+    pub fn load(&mut self, path: &str) {
+        let file_path = std::env::current_dir()
+            .expect("Failed to get current folder")
+            .join(path);
         let json_open = std::fs::File::open(file_path).expect("Unable to open json file");
-        let json_data:JsonGlobalInfo = serde_json::from_reader(json_open).expect("error while reading or parsing the json");
-    
-        for data in json_data.magicbytes{
+        let json_data: JsonGlobalInfo =
+            serde_json::from_reader(json_open).expect("error while reading or parsing the json");
+
+        for data in json_data.magicbytes {
             self.magicbyte.insert(data.name, data.magicbyte);
         }
 
-        for data in json_data.payloads{
-            let tmp_data:PayloadPathInfo = {PayloadPathInfo{needroot:data.needroot,file_type:data.file_type,fullpath:data.fullpath, cmd_var:data.cmd_var, cmd_path:data.cmd_path}};
+        for data in json_data.payloads {
+            let tmp_data: PayloadPathInfo = {
+                PayloadPathInfo {
+                    needroot: data.needroot,
+                    file_type: data.file_type,
+                    fullpath: data.fullpath,
+                    cmd_var: data.cmd_var,
+                    cmd_path: data.cmd_path,
+                }
+            };
             self.payload.insert(data.name, tmp_data);
         }
 
-        for data in json_data.ads{
+        for data in json_data.ads {
             self.ads_adsname.insert(data.name.clone(), data.adsname);
-            self.ads_hexvalue.insert(data.name,data.hexvalue);
+            self.ads_hexvalue.insert(data.name, data.hexvalue);
         }
-
     }
 
-    pub fn file_magicbyte_list(&self)->HashSet<String>{
+    pub fn file_magicbyte_list(&self) -> HashSet<String> {
         self.magicbyte.keys().cloned().collect()
     }
-   
-    pub fn file_magicbyte_exist(&self,name:&str)->bool{
+
+    pub fn file_magicbyte_exist(&self, name: &str) -> bool {
         self.magicbyte.contains_key(name)
     }
 
-    pub fn file_magicbyte_get(&self,name:&str)-> Vec<u8>{
+    pub fn file_magicbyte_get(&self, name: &str) -> Vec<u8> {
         let mut payload = "467261636b313133";
-        if self.file_magicbyte_exist(name){
+        if self.file_magicbyte_exist(name) {
             payload = self.magicbyte.get(name).clone().unwrap(); //Can not faild as the key exist
         }
         let header = tools_generator::hex_to_bytes(payload); // User input 😅
-        match header{
+        match header {
             Some(data) => data,
-            None => vec![70,114,97,99,107,49,49,51],
+            None => vec![70, 114, 97, 99, 107, 49, 49, 51],
         }
     }
 
-    pub fn file_payload_list(&self)->HashSet<String>{
+    pub fn file_payload_list(&self) -> HashSet<String> {
         self.payload.keys().cloned().collect()
     }
 
-    pub fn file_payload_exist(&self,name:&str)->bool{
+    pub fn file_payload_exist(&self, name: &str) -> bool {
         self.payload.contains_key(name)
     }
 
-    pub fn file_payload_needroot(&self,name:&str)->bool{
-        let data = self.payload.get(name).unwrap(); // exit fail if name is invalid 
+    pub fn file_payload_needroot(&self, name: &str) -> bool {
+        let data = self.payload.get(name).unwrap(); // exit fail if name is invalid
         data.needroot
     }
 
-    pub fn file_payload_getfilename(&self,name:&str)->String{
-        if self.file_payload_exist(name){
+    pub fn file_payload_getfilename(&self, name: &str) -> String {
+        if self.file_payload_exist(name) {
             let data = self.payload.get(name).unwrap(); // name is a valid key
-            
-            if data.fullpath.len() >0 {
+
+            if data.fullpath.len() > 0 {
                 tools_generator::regex_to_string(&data.fullpath)
             } else {
                 let filename = tools_generator::regex_to_string(&data.cmd_path);
-                let var_path= env::var_os(&data.cmd_var).unwrap();
+                let var_path = env::var_os(&data.cmd_var).unwrap();
                 let full_path = std::path::Path::new(&var_path).join(filename);
                 String::from(full_path.to_string_lossy())
             }
-            
         } else {
             "c:/wag.file".to_string()
         }
     }
 
-    pub fn file_payload_getfiletype(&self,name:&str)->String{
-        if self.file_payload_exist(name){
+    pub fn file_payload_getfiletype(&self, name: &str) -> String {
+        if self.file_payload_exist(name) {
             let data = self.payload.get(name).unwrap(); // name is a valid key
             data.file_type.clone()
-                        
         } else {
             "Wag".to_string()
         }
     }
 
-    pub fn file_ads_list(&self)->HashSet<String>{
+    pub fn file_ads_list(&self) -> HashSet<String> {
         self.ads_adsname.keys().cloned().collect()
     }
 
-    pub fn file_ads_exist(&self,name:&str)->bool{
+    pub fn file_ads_exist(&self, name: &str) -> bool {
         self.ads_adsname.contains_key(name)
     }
 
-    pub fn file_ads_get_data(&self,name:&str)->Vec<u8>{
-        println!("Ask for {}",name);
+    pub fn file_ads_get_data(&self, name: &str) -> Vec<u8> {
+        println!("Ask for {}", name);
         let payload = self.ads_hexvalue.get(name).clone().unwrap(); //Can not faild as the key exist
-        println!("{}",payload);
+        println!("{}", payload);
         let header = tools_generator::hex_to_bytes(payload); // User input 😅
-        match header{
+        match header {
             Some(data) => data,
-            None => vec![70,114,97,99,107,49,49,51],
+            None => vec![70, 114, 97, 99, 107, 49, 49, 51],
         }
     }
 
-    pub fn file_ads_get_name(&self,name:&str)->String{
+    pub fn file_ads_get_name(&self, name: &str) -> String {
         let data = self.ads_adsname.get(name).clone().unwrap(); //Can not faild as the key exist
         data.to_string()
     }
-
 }
--- src/tools_cli.rs
+++ src/tools_cli.rs
@@ -16,60 +16,132 @@ pub enum Clioptions {
     /// Alternate Data Stream
     #[clap(arg_required_else_help = true)]
     ADS {
-        #[clap(short = 'f', long, required = false,default_value ="", help="Full path filename (regex)")]
+        #[clap(
+            short = 'f',
+            long,
+            required = false,
+            default_value = "",
+            help = "Full path filename (regex)"
+        )]
         filename: String,
-        #[clap(short = 'a', long, required = false,default_value ="", help="ADS to use")]
-        adsname: String, 
-        #[clap(short = 'l', long, required = false, default_value_t = false,help="Get all the possible ADS name and quit")]
+        #[clap(
+            short = 'a',
+            long,
+            required = false,
+            default_value = "",
+            help = "ADS to use"
+        )]
+        adsname: String,
+        #[clap(
+            short = 'l',
+            long,
+            required = false,
+            default_value_t = false,
+            help = "Get all the possible ADS name and quit"
+        )]
         list: bool,
     },
 
     /// Bring Your Own Vulnerable Driver
     #[clap(arg_required_else_help = true)]
     BYOVD {
-        #[clap(short = 'n', long, help="Internal Name of the service")]
+        #[clap(short = 'n', long, help = "Internal Name of the service")]
         name: String,
-        #[clap(short = 'd', long, help="Displayed Name of the service")]
+        #[clap(short = 'd', long, help = "Displayed Name of the service")]
         details: String,
-        #[clap(short = 'p', long, help="Full path to the driver eg: c:\\temp...")]
+        #[clap(short = 'p', long, help = "Full path to the driver eg: c:\\temp...")]
         path: String,
     },
 
     /// Create dummy file Artefact
     #[clap(arg_required_else_help = true)]
     FileCreateAuto {
-        #[clap(short = 'n', long, required = false,default_value ="", help="Name of the malware to mimic")]
+        #[clap(
+            short = 'n',
+            long,
+            required = false,
+            default_value = "",
+            help = "Name of the malware to mimic"
+        )]
         name: String,
-        #[clap(short = 'm', long, required = false, default_value_t = false,help="Get all the possible mimic name and quit")]
+        #[clap(
+            short = 'm',
+            long,
+            required = false,
+            default_value_t = false,
+            help = "Get all the possible mimic name and quit"
+        )]
         mimic: bool,
     },
     /// Create dummy file Artefact manualy
     #[clap(arg_required_else_help = true)]
     FileCreateManual {
-        #[clap(short = 'f', long, required = false,default_value ="", help="Full path filename (regex)")]
+        #[clap(
+            short = 'f',
+            long,
+            required = false,
+            default_value = "",
+            help = "Full path filename (regex)"
+        )]
         filename: String,
-        #[clap(short = 'm', long, required = false,default_value ="", help="MagicBytes name to use")]
-        magicbyte: String,       
-        #[clap(short = 'l', long, required = false, default_value_t = false,help="Get all the possible MagicBytes name and quit")]
+        #[clap(
+            short = 'm',
+            long,
+            required = false,
+            default_value = "",
+            help = "MagicBytes name to use"
+        )]
+        magicbyte: String,
+        #[clap(
+            short = 'l',
+            long,
+            required = false,
+            default_value_t = false,
+            help = "Get all the possible MagicBytes name and quit"
+        )]
         list: bool,
     },
 
     /// Generates Name Pipe Artefact
     #[clap(arg_required_else_help = true)]
     NamePipeAuto {
-        #[clap(short = 'n', long, required = false,default_value ="", help="Name of the malware to mimic")]
+        #[clap(
+            short = 'n',
+            long,
+            required = false,
+            default_value = "",
+            help = "Name of the malware to mimic"
+        )]
         name: String,
         #[clap(short = 't', long, required = false, default_value_t = 0)]
         number: usize,
-        #[clap(short = 'p', long, required = false, default_value_t = false,help="Get all the possible pipename for a mimic and quit")]
+        #[clap(
+            short = 'p',
+            long,
+            required = false,
+            default_value_t = false,
+            help = "Get all the possible pipename for a mimic and quit"
+        )]
         pipe: bool,
-        #[clap(short = 'm', long, required = false, default_value_t = false,help="Get all the possible mimic name and quit")]
+        #[clap(
+            short = 'm',
+            long,
+            required = false,
+            default_value_t = false,
+            help = "Get all the possible mimic name and quit"
+        )]
         mimic: bool,
     },
     /// Generates Name Pipe Artefact manualy
     #[clap(arg_required_else_help = true)]
     NamePipeManual {
-        #[clap(short = 'p', long, required = true,default_value ="", help="Regex of the PipeName to Create")]
+        #[clap(
+            short = 'p',
+            long,
+            required = true,
+            default_value = "",
+            help = "Regex of the PipeName to Create"
+        )]
         name: String,
     },
 }
