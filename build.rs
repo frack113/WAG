11e5e6e8bc5d5113440a90ceb71cbfb8db5090c7
frack113 <62423083+frack113@users.noreply.github.com>
2024-08-28 06:43:34 +0200

    chore: 🧹 Apply review

--- build.rs
+++ build.rs
@@ -6,34 +6,3 @@ extern crate embed_resource;
 fn main() {
     embed_resource::compile("wag-manifest.rc", embed_resource::NONE);
 }
-
-// use winres::{self, WindowsResource};
-
-// fn main() {
-//     if cfg!(target_os = "windows") {
-//         let mut res: WindowsResource = winres::WindowsResource::new();
-
-//         res.set("FileVersion", "1.0.0");
-//         res.set("InternalName", "🔓 your detection");
-//         res.set("CompanyName", "Frack113");
-//         res.set(
-//             "FileDescription",
-//             "Generate Some Windows Artefact to 👊 your EDR",
-//         );
-//         res.set("LegalCopyright", "Frack113");
-//         res.set("LegalTrademark", "Frack113");
-//         res.set("OriginalFilename", "wag.exe");
-//         res.set("ProductName", "WindowsArtefactGenerator");
-//         res.set("ProductVersion", "0.1.0");
-//         res.set("Comments", "Catch me if you can😁");
-
-//         const LANG_NEUTRAL: u16 = 0x00;
-//         const SUBLANG_NEUTRAL: u16 = 0x00;
-
-//         res.set_language((LANG_NEUTRAL << 10) | SUBLANG_NEUTRAL);
-
-//         res.set_icon("./media/wag.ico");
-
-//         res.compile().unwrap();
-//     }
-// }
