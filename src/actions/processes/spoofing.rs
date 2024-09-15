21f325392f0229a4736226441ed8b638f8eb3aac
AntwortEinesLebens <AntwortEinesLebens@proton.me>
2024-09-15 21:56:50 +0200

    feat: ✨ Rewrite spoofing and don't close the process that was created

--- src/actions/processes/spoofing.rs
+++ src/actions/processes/spoofing.rs
@@ -2,26 +2,18 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-// PPID Spoofing
-//
-// Last update 20240224
-
 use crate::{actions::Runnable, windows::processes::get_pid};
 use clap::Parser;
-use core::ffi::c_void;
-use std::{error::Error, mem::size_of, thread, time::Duration};
+use std::{error::Error, ffi::OsString, iter::once, mem::size_of, os::windows::ffi::OsStrExt};
 use windows::{
-    core::PSTR,
+    core::{Owned, PCWSTR, PWSTR},
     Win32::{
-        Foundation::{CloseHandle, HANDLE},
-        System::{
-            Memory::{GetProcessHeap, HeapAlloc, HEAP_FLAGS},
-            Threading::{
-                CreateProcessA, InitializeProcThreadAttributeList, OpenProcess, TerminateProcess,
-                UpdateProcThreadAttribute, WaitForSingleObject, LPPROC_THREAD_ATTRIBUTE_LIST,
-                PROCESS_ACCESS_RIGHTS, PROCESS_CREATION_FLAGS, PROCESS_INFORMATION,
-                PROC_THREAD_ATTRIBUTE_PARENT_PROCESS, STARTF_USESHOWWINDOW, STARTUPINFOEXA,
-            },
+        Foundation::HANDLE,
+        System::Threading::{
+            CreateProcessW, InitializeProcThreadAttributeList, OpenProcess,
+            UpdateProcThreadAttribute, EXTENDED_STARTUPINFO_PRESENT, LPPROC_THREAD_ATTRIBUTE_LIST,
+            PROCESS_INFORMATION, PROCESS_SET_INFORMATION, PROC_THREAD_ATTRIBUTE_PARENT_PROCESS,
+            STARTUPINFOEXW, STARTUPINFOW,
         },
     },
 };
@@ -39,83 +31,75 @@ pub struct Spoofing {
     parent_executable: String,
 }
 
-fn create_ppid(name: &String, new_ppid: u32) -> bool {
-    println!("Use the PPID {}", new_ppid);
-    println!("Open the Parent Process");
-    let mut parent_process_handle: HANDLE =
-        unsafe { OpenProcess(PROCESS_ACCESS_RIGHTS(0x02000000), false, new_ppid).unwrap() };
+fn spoof(executable: &str, parent_pid: u32) -> Result<(), Box<dyn Error>> {
+    let mut required_size: usize = 0;
 
-    let mut pi: PROCESS_INFORMATION = PROCESS_INFORMATION::default();
-    let mut sinfo: STARTUPINFOEXA = STARTUPINFOEXA::default();
-    let mut cb_attribute_list_size: usize = size_of::<STARTUPINFOEXA>();
-    sinfo.StartupInfo.cb = cb_attribute_list_size as u32;
-    sinfo.StartupInfo.dwFlags = STARTF_USESHOWWINDOW;
-
-    println!("allocate memory for PROC_THREAD_ATTRIBUTE_LIST");
-    sinfo.lpAttributeList = LPPROC_THREAD_ATTRIBUTE_LIST(unsafe {
-        HeapAlloc(
-            GetProcessHeap().unwrap(),
-            HEAP_FLAGS(0),
-            cb_attribute_list_size,
-        )
-    });
-
-    println!("InitializeProcThreadAttributeList");
     unsafe {
-        InitializeProcThreadAttributeList(sinfo.lpAttributeList, 1, 0, &mut cb_attribute_list_size)
-            .unwrap()
+        let _ = InitializeProcThreadAttributeList(
+            LPPROC_THREAD_ATTRIBUTE_LIST::default(),
+            1,
+            0,
+            &mut required_size,
+        );
     };
 
-    println!("UpdateProcThreadAttribute");
-    let _ = unsafe {
+    let mut attributes: Box<[u8]> = vec![0; required_size].into_boxed_slice();
+    let startup_informations: STARTUPINFOEXW = STARTUPINFOEXW {
+        StartupInfo: STARTUPINFOW {
+            cb: size_of::<STARTUPINFOEXW>() as u32,
+            ..Default::default()
+        },
+        lpAttributeList: LPPROC_THREAD_ATTRIBUTE_LIST(attributes.as_mut_ptr() as *mut _),
+    };
+
+    unsafe {
+        InitializeProcThreadAttributeList(
+            startup_informations.lpAttributeList,
+            1,
+            0,
+            &mut required_size,
+        )?;
+
         UpdateProcThreadAttribute(
-            sinfo.lpAttributeList,
+            startup_informations.lpAttributeList,
             0,
             PROC_THREAD_ATTRIBUTE_PARENT_PROCESS as usize,
-            Some(&mut parent_process_handle as *mut _ as *mut c_void),
+            Some(
+                &mut *Owned::new(OpenProcess(PROCESS_SET_INFORMATION, false, parent_pid)?) as *mut _
+                    as *mut _,
+            ),
             size_of::<HANDLE>(),
             None,
             None,
-        )
-    };
+        )?;
 
-    println!("CreateProcessA");
-    let process_name = format!("{}\0", name);
-    let new_process = unsafe {
-        CreateProcessA(
-            None,
-            PSTR::from_raw(process_name.to_owned().as_mut_ptr()),
+        CreateProcessW(
+            PCWSTR::null(),
+            PWSTR(
+                OsString::from(executable)
+                    .encode_wide()
+                    .chain(once(0))
+                    .collect::<Vec<_>>()
+                    .as_mut_ptr(),
+            ),
             None,
             None,
             false,
-            PROCESS_CREATION_FLAGS(0x00080000),
+            EXTENDED_STARTUPINFO_PRESENT,
             None,
-            None,
-            &sinfo.StartupInfo,
-            &mut pi,
-        )
+            PCWSTR::null(),
+            &startup_informations.StartupInfo,
+            &mut PROCESS_INFORMATION::default(),
+        )?
     };
-    match new_process {
-        Ok(_) => {
-            println!("New process is created with pid {:}", pi.dwProcessId);
-            let wait_duration: Duration = Duration::from_millis(2000);
-            thread::sleep(wait_duration);
-            let _ = unsafe { TerminateProcess(pi.hProcess, 0) };
-            let _ = unsafe { WaitForSingleObject(pi.hProcess, 5000) };
-            let _ = unsafe { CloseHandle(pi.hProcess) };
-            let _ = unsafe { CloseHandle(pi.hThread) };
-            true
-        }
-        Err(_) => false,
-    }
+
+    Ok(())
 }
 
 impl Runnable for Spoofing {
-    /* Version 20240209 */
     fn run(&self) -> Result<i32, Box<dyn Error>> {
-        println!("PPID spoofing");
-        let result: bool = create_ppid(&self.executable, get_pid(&self.parent_executable)?);
+        spoof(&self.executable, get_pid(&self.parent_executable)?)?;
 
-        Ok(!result as i32)
+        Ok(0)
     }
 }
